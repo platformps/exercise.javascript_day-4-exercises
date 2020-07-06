82b60ab86a27131d4eab68b6c389b8494f5b5dfc
mondira-roy2020 <mondira.roy.git2020@gmail.com>
2020-07-06 14:55:32 -0400

    Slide 44 works GREAT!!!

--- js/manual-animation.js
+++ js/manual-animation.js
@@ -20,27 +20,34 @@ function backToNormal(eventTarget) {
 
 function moveImage(direction) {
     let imgObjStyle = imgObj.style;
-    let imgObjStyleDirection;
+    //let imgObjStyleDirection;
+    var topVal = parseInt(imgObjStyle.top, 10);
+    var leftVal = parseInt(imgObjStyle.left, 10);
 
     if(direction === 'KeyD') {        
-        imgObjStyleDirection = imgObjStyle.left;
-        imgObjStyleDirection = parseInt(imgObjStyleDirection) + 10 + 'px';
+        // imgObjStyleDirection = imgObjStyle.left;
+        // imgObjStyleDirection = parseInt(imgObjStyleDirection) + 10 + 'px';
+        imgObjStyle.left = (leftVal + 30) + "px";
+       
     }
 
     if(direction === 'KeyA') {
-        imgObjStyleDirection = imgObjStyle.left;
-        imgObjStyleDirection = parseInt(imgObjStyleDirection) - 10 + 'px';
+        //imgObjStyleDirection = imgObjStyle.left;
+        //imgObjStyleDirection = parseInt(imgObjStyleDirection) - 10 + 'px';
+        imgObjStyle.top = (topVal - 30) + "px";
     }
 
     if(direction === 'KeyW') {
-        imgObjStyleDirection = imgObjStyle.top;
-        imgObjStyleDirection = parseInt(imgObjStyleDirection) - 10 + 'px';
+        //imgObjStyleDirection = imgObjStyle.top;
+        //imgObjStyleDirection = parseInt(imgObjStyleDirection) - 10 + 'px';
+        imgObjStyle.left = (leftVal - 30) + "px";
     }
 
     
     if(direction === 'KeyS') {
-        imgObjStyleDirection = imgObjStyle.top;
-        imgObjStyleDirection = parseInt(imgObjStyleDirection) + 10 + 'px';
+        //imgObjStyleDirection = imgObjStyle.top;
+        //imgObjStyleDirection = parseInt(imgObjStyleDirection) + 10 + 'px';
+        imgObjStyle.top = (topVal + 30) + "px";
     }
 }
 
