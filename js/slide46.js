4f8e75f95c8fbd628bb0d1550d22926aee490a56
suresh kandagatla <surikandagatla@gmail.com>
2020-07-06 15:04:13 -0400

    Finished slides 45 and 46. Included both in index.html for reference

---
+++ js/slide46.js
@@ -0,0 +1,60 @@
+
+var animate;
+var direction;
+const buttons = document.querySelectorAll("button");
+for(var i=0; i<buttons.length;i++) {
+    let currentButton = buttons[i];
+    currentButton.addEventListener('mouseenter', () => changeToCoral(event.target));
+    currentButton.addEventListener('mouseleave', () => backToNormal(event.target));
+    currentButton.addEventListener('click', function(){direction = event.target.id;
+    animate = setInterval(function() {moveImage(direction);}, 100);});
+    document.addEventListener('mouseup', () => {clearInterval(animate);
+    });
+   
+}
+
+function changeToCoral(eventTarget) {
+    eventTarget.style.backgroundColor = "coral";
+}
+
+function changeToWhite(eventTarget) {
+    eventTarget.style.backgroundColor = "white";
+}
+
+function backToNormal(eventTarget) {
+    eventTarget.style.backgroundColor='';
+}
+
+function moveImage(direction) {
+    let imgObjStyle = imgObj.style;
+   // let imgObjStyleDirection;
+
+    if(direction === 'KeyD') {        
+        imgObjStyle.left = parseInt(imgObjStyle.left) + 10 + 'px';
+    }
+
+    if(direction === 'KeyA') {
+        imgObjStyle.left = parseInt(imgObjStyle.left) - 10 + 'px';
+    }
+
+    if(direction === 'KeyW') {
+        imgObjStyle.top = parseInt(imgObjStyle.top) - 10 + 'px';
+    }
+
+    
+    if(direction === 'KeyS') {
+        imgObjStyle.top = parseInt(imgObjStyle.top) + 10 + 'px';
+    }
+    animate = setTimeout(function(){moveImage(direction);}, 20);
+}
+
+
+function init() {
+    imgObj = document.getElementById('myImage');
+    imgObj.style.position = 'relative';
+    imgObj.style.left = '0px';
+    imgObj.style.top = '0px';
+}
+
+
+window.onload = init;
