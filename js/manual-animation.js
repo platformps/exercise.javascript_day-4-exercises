e5d615235debd4362f34fb8b2831387162df30e5
Steven Snyder <sts12589@gmail.com>
2020-07-06 13:45:54 -0400

    keypress events added

--- js/manual-animation.js
+++ js/manual-animation.js
@@ -18,33 +18,61 @@ function backToNormal(eventTarget) {
     eventTarget.style.backgroundColor='';
 }
 
+
 function moveImage(direction) {
-    let imgObjStyle = imgObj.style;
+    var imgObjStyle = imgObj.style;
     var topVal = parseInt(imgObjStyle.top, 10);
     var leftVal = parseInt(imgObjStyle.left, 10);
 
-    if(direction === 'KeyD') {        
+    if(direction === 'KeyA') {        
         imgObjStyle.left = (leftVal - 30) + "px";
     }
 
-    if(direction === 'KeyA') {
-        
+    if(direction === 'KeyW') {
         imgObjStyle.top = (topVal - 30) + "px";
     }
 
-    if(direction === 'KeyW') {
-
-    imgObjStyle.left = (leftVal + 30) + "px";
-
+    if(direction === 'KeyD') {
+        imgObjStyle.left = (leftVal + 30) + "px";
     }
 
-    
     if(direction === 'KeyS') {
         imgObjStyle.top = (topVal + 30) + "px";
     }
+
+    document.addEventListener('keydown', function(e){
+        if(e.key === 'w')
+        console.log('hei w')
+     })
+}
+
+function keyMoves(e) {
+    var imgObjStyle = imgObj.style;
+    var topVal = parseInt(imgObjStyle.top, 10);
+    var leftVal = parseInt(imgObjStyle.left, 10);
+
+    switch (e.key) {
+        case 'a':
+            imgObjStyle.left = (leftVal - 30) + "px";
+            break;
+        case 'w':
+            imgObjStyle.top = (topVal - 30) + "px";
+            break;
+        case 'd':
+            imgObjStyle.left = (leftVal + 30) + "px";
+            break;
+        case 's':
+            imgObjStyle.top = (topVal + 30) + "px";
+            break;
+    
+        default:
+            break;
+    }
+    
 }
 
 function init() {
+    document.addEventListener("keydown", keyMoves);
     imgObj = document.getElementById('myImage');
     imgObj.style.position = 'relative';
     imgObj.style.left = '0px';
