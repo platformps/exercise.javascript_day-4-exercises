e229534d842bdab8db086a48e9ddd4aab6125b18
Calvin Quinn <caoyuru03@gmail.com>
2020-07-06 15:23:11 -0400

    completed ex

--- js/manual-animation.js
+++ js/manual-animation.js
@@ -1,4 +1,6 @@
 const buttons = document.querySelectorAll("button");
+var animate;
+var timer = 0;
 for(var i=0; i<buttons.length;i++) {
     let currentButton = buttons[i];
     currentButton.addEventListener('mouseenter', () => changeToCoral(event.target));
@@ -18,30 +20,61 @@ function backToNormal(eventTarget) {
     eventTarget.style.backgroundColor='';
 }
 
+document.addEventListener('mouseup',()=>{clearTimeout(animate);}) //click anywhere to stop moveImage
+document.addEventListener('keydown',(event)=> {
+  switch(event.keyCode){
+    case 65: {
+      moveImage('KeyA');
+      break;
+    }
+    case 68: {
+      moveImage('KeyD');
+      break;
+    }
+    case 87: {
+      moveImage('KeyW');
+      break;
+    }
+    case 83: {
+      moveImage('KeyS');
+      break;
+    }
+  }
+
+});
+
 function moveImage(direction) {
+    clearTimeout(animate);
+    clearTimeout(timer);
     let imgObjStyle = imgObj.style;
     let imgObjStyleDirection;
 
     if(direction === 'KeyD') {        
-        imgObjStyleDirection = imgObjStyle.left;
-        imgObjStyleDirection = parseInt(imgObjStyleDirection) + 10 + 'px';
+        imgObjStyle.left = parseInt(imgObjStyle.left) + 10 + 'px';
     }
 
-    if(direction === 'KeyA') {
-        imgObjStyleDirection = imgObjStyle.left;
-        imgObjStyleDirection = parseInt(imgObjStyleDirection) - 10 + 'px';
+    else if(direction === 'KeyA') {
+        imgObjStyle.left = parseInt(imgObjStyle.left) - 10 + 'px';
     }
 
-    if(direction === 'KeyW') {
-        imgObjStyleDirection = imgObjStyle.top;
-        imgObjStyleDirection = parseInt(imgObjStyleDirection) - 10 + 'px';
+    else if(direction === 'KeyW') {
+        imgObjStyle.top = parseInt(imgObjStyle.top) - 10 + 'px';
     }
 
-    
-    if(direction === 'KeyS') {
-        imgObjStyleDirection = imgObjStyle.top;
-        imgObjStyleDirection = parseInt(imgObjStyleDirection) + 10 + 'px';
+    else if(direction === 'KeyS') {
+        imgObjStyle.top = parseInt(imgObjStyle.top) + 10 + 'px';
     }
+
+    animate = setTimeout(() => { //every time set up animate timeout, timer var will accumulate
+    timer += 20;  //every time press a key, the timeout will reinitiate
+    moveImage(direction);
+    if(timer > 1000) { //pass 1000 so that its stay in the boundary of browser
+    clearTimeout(animate)
+    timer = 0;
+    }
+    },20)
+
+   
 }
 
 function init() {
