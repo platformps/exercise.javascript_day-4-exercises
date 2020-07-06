9f7124038e4d639d3f8e880564cfd7495edd3e2d
AnjaliG927 <anjali.ghaligi@gmail.com>
2020-07-06 15:48:02 -0400

    slide 45

---
+++ js/automated-animation.js
@@ -0,0 +1,55 @@
+var animate;
+const buttons = document.querySelectorAll("button");
+for (var i = 0; i < buttons.length; i++) {
+    let currentButton = buttons[i];
+    // currentButton.addEventListener('mouseenter', () => changeToCoral(event.target));
+    // currentButton.addEventListener('mouseleave', () => backToNormal(event.target));
+    currentButton.addEventListener('click', () => moveImage(event.target.id))
+    document.addEventListener('mouseup', function () { });
+}
+
+function changeToCoral(eventTarget) {
+    eventTarget.style.backgroundColor = "coral";
+}
+
+function changeToWhite() {
+    eventTarget.style.backgroundColor = "white";
+}
+
+function backToNormal(eventTarget) {
+    eventTarget.style.backgroundColor = '';
+}
+
+function moveImage(direction) {
+    let imgObjStyle = imgObj.style;
+    let topVal = parseInt(imgObjStyle.top, 10);
+    let leftVal = parseInt(imgObjStyle.left, 10);
+
+    if (direction === 'KeyA') {
+        imgObjStyle.left = (leftVal - 30) + "px";
+    }
+
+    if (direction === 'KeyW') {
+        imgObjStyle.top = (topVal - 30) + "px";
+    }
+
+    if (direction === 'KeyD') {
+        imgObjStyle.left = (leftVal + 30) + "px";
+    }
+
+
+    if (direction === 'KeyS') {
+        imgObjStyle.top = (topVal + 30) + "px";
+    }
+    animate = setTimeout(function () { moveImage(direction) }, 20);
+}
+
+function init() {
+    imgObj = document.getElementById('myImage');
+    imgObj.style.position = 'relative';
+    imgObj.style.left = '0px';
+    imgObj.style.top = '0px';
+}
+
+
+window.onload = init;
