266882c2f6bab7ce872f3b89eaf8bf328c39355b
Julia Waclawek <julie.waclawek@gmail.com>
2020-07-06 23:36:44 -0400

    I have added changes

--- js/manual-animation.js
+++ js/manual-animation.js
@@ -1,9 +1,11 @@
-const buttons = document.querySelectorAll("button");
+var animate
+const buttons = document.querySelectorAll('button');
+
 for(var i=0; i<buttons.length;i++) {
     let currentButton = buttons[i];
     currentButton.addEventListener('mouseenter', () => changeToCoral(event.target));
     currentButton.addEventListener('mouseleave', () => backToNormal(event.target));
-    currentButton.addEventListener('click', ()=> moveImage(event.target.id))
+    currentButton.addEventListener('click', ()=> moveImage(event.target.id));
 }
 
 function changeToCoral(eventTarget) {
@@ -25,30 +27,35 @@ function moveImage(direction) {
     if(direction === 'KeyD') {        
         imgObjStyleDirection = imgObjStyle.left;
         imgObjStyleDirection = parseInt(imgObjStyleDirection) + 10 + 'px';
+        imgObjStyle.left = (leftVal + 30) + "px";
     }
+    let topVal = parseInt(imgObjStyle.top, 10);
+    let leftVal = parseInt(imgObjStyle.left, 10);
 
     if(direction === 'KeyA') {
         imgObjStyleDirection = imgObjStyle.left;
         imgObjStyleDirection = parseInt(imgObjStyleDirection) - 10 + 'px';
-    }
-
+        imgObjStyle.left = (leftVal - 30) + "px";
+    } 
+    
     if(direction === 'KeyW') {
         imgObjStyleDirection = imgObjStyle.top;
         imgObjStyleDirection = parseInt(imgObjStyleDirection) - 10 + 'px';
+        imgObjStyle.top = (topVal - 30) + "px";
     }
 
-    
     if(direction === 'KeyS') {
         imgObjStyleDirection = imgObjStyle.top;
         imgObjStyleDirection = parseInt(imgObjStyleDirection) + 10 + 'px';
+        imgObjStyle.top = (topVal + 30) + "px";
     }
 }
 
 function init() {
     imgObj = document.getElementById('myImage');
     imgObj.style.position = 'relative';
-    imgObj.style.left = '0px';
-    imgObj.style.top = '0px';
+    imgObj.style.left = '30px';
+    imgObj.style.top = '30px';
 }
 
 
